d44317833d1db301635c0549d5de26213cc8a1b5
antonio-cod <antonio_carlostads@hotmail.com>
2025-03-14 11:54:52 -0400

    Listing Groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,32 @@
 import { useState } from "react"
+import { FlatList } from "react-native"
 import { Group } from "@components/Group"
 
 import { HomeHeader } from "@components/HomeHeader"
 import { Center, Text, VStack } from "@gluestack-ui/themed"
 
 export function Home() {
+  const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "Ombro"])
   const [groupSelected, setGroupSelected] = useState("costa")
 
   return (
     <VStack flex={1}>
        <HomeHeader />
-          <Group 
-            name="Costas"
-            isActive={ groupSelected === "costa"}
-            onPress={() => setGroupSelected("costa")}
-           />
-
-          <Group 
-            name="Ombro"
-            isActive={ groupSelected === "ombro"}
-            onPress={() => setGroupSelected("ombro")}
-          />
+       <FlatList 
+          data={groups}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Group 
+              name={item}
+              isActive={ groupSelected === item}
+              onPress={() => setGroupSelected(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 32 }}
+          style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+        />
     </VStack>
   )
 }
