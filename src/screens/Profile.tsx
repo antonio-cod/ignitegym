e32c6616f6fb34c508ea2fa3215a5887a556e3e5
antonio-cod <antonio_carlostads@hotmail.com>
2025-03-15 03:18:30 -0400

    User Photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,23 @@
+import { ScrollView } from "react-native"
 import { ScreenHeader } from "@components/ScreenHeader"
-import { VStack, Text } from "@gluestack-ui/themed"
+
+import { VStack, Text, Center } from "@gluestack-ui/themed"
+import { UserPhoto } from "@components/UserPhoto"
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto 
+            source={{ uri: "https://avatars.githubusercontent.com/u/67842667?v=4 "}}
+            alt="Foto do usuário"
+            size="xl"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
