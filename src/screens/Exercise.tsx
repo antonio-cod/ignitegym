1bd2e85483d77cbf193606b9167067c0c4042788
antonio-cod <antonio_carlostads@hotmail.com>
2025-03-17 14:18:16 -0400

    Finishing the Exercise Header

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,10 +1,12 @@
 import { TouchableOpacity } from "react-native"
 
-import { Icon } from "@gluestack-ui/themed"
+import { HStack, Heading, Icon } from "@gluestack-ui/themed"
 import { VStack, Text } from "@gluestack-ui/themed"
 import { useNavigation } from "@react-navigation/native"
 
 import { ArrowLeft } from "lucide-react-native"
+
+import BodySvg from "@assets/body.svg"
 
 import { AppNavigatorRoutesProps } from "@routes/app.routes"
 
@@ -21,6 +23,30 @@ export function Exercise() {
       <TouchableOpacity onPress={handleGoBack}>
         <Icon as={ArrowLeft} color="$green500" size="xl" />
       </TouchableOpacity>
+
+      <HStack
+        justifyContent="space-between"
+        alignItems="center"
+        mt="$4"
+        mb="$8"
+      >
+        <Heading
+          color="$gray100"
+          fontFamily="$heading"
+          fontSize="$lg"
+          flexShrink={1}
+        >
+          Puxada Frontal
+        </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="$gray200" ml="$1" textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+      </HStack>
     </VStack>
 
     </VStack>
