66b3bf8e0bed74875990c73635a8cd40942115c9
antonio-cod <antonio_carlostads@hotmail.com>
2025-04-09 13:55:44 -0400

    Validating Password and Confirming Password

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -19,12 +19,20 @@ type FormDataProps = {
 
 const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome'),
-  email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
+  password: yup
+  .string()
+  .required('Informe a senha')
+  .min(6, 'A senha deve conter minimo 6 digitos!'),
+  password_confirm: yup
+  .string()
+  .required('Confirme a senha.')
+  .oneOf([yup.ref("password"), ""], "A confirmação da senha não confere."),
 });
 
 export function SignUp() {
  
-  const { control, handleSubmit, formState: {errors} } = useForm<FormDataProps>({
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema)
   });
 
@@ -122,18 +130,15 @@ export function SignUp() {
           <Controller
             control={control}
             name="password_confirm"
-            rules={{
-              required: "Confirme a senha.",
-            }}
             render={({ field: { onChange, value }}) => (
               <Input
                 placeholder="Confirme a Senha"
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
-                errorMessage={errors.password?.message}
                 onSubmitEditing={handleSubmit(handleSignUp)}
                 returnKeyType="send"
+                errorMessage={errors.password_confirm?.message}
                 />
             )}
           />
