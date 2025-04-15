b1e5c7c579b4b0ea80323e6d246a5ff7bb35058a
antonio-cod <antonio_carlostads@hotmail.com>
2025-04-15 17:19:21 -0400

    Using Axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,6 +4,8 @@ import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
+import { api } from  '@services/api';
+
 import backgroundImg from '@assets/background.png';
 import Logo from '@assets/logo.svg';
 
@@ -43,16 +45,20 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch('http://192.168.1.5:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password })
-    });
-    const data = await response.json();
-    console.log(data);
+
+    const response = await api.post('/users', {name, email, password});
+    console.log(response.data);
+
+    // const response = await fetch('http://192.168.1.5:3333/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ name, email, password })
+    // });
+    // const data = await response.json();
+    // console.log(data);
     
   }
 
