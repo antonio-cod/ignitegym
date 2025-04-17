60dd44cfeb5a6c661e732338d382dc49c6f82a9e
antonio-cod <antonio_carlostads@hotmail.com>
2025-04-17 19:53:29 -0400

    Creating your own Hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,22 @@
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import { useContext } from "react"
 
-import { AuthContext } from "@contexts/AuthContext"
+
 
  import { AuthRoutes } from "./auth.routes"
 
 import { Box } from "@gluestack-ui/themed"
 
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
+import { useAuth } from "@hooks/useAuth"
 
 export function Routes() {
   const theme = DefaultTheme
+  const { user } = useAuth();
+  
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
-  const contextData = useContext(AuthContext);
-  console.log("Usuario logado", contextData);
+ 
   
   return (
     <Box flex={1} bg="$gray700">
