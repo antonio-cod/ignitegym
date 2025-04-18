481337682a5f1a39d09659041cf6f3a5927a50c5
antonio-cod <antonio_carlostads@hotmail.com>
2025-04-17 20:40:02 -0400

    Recap of the SignIn form

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,74 +1,107 @@
+import { Controller, useForm } from 'react-hook-form'
+import { useNavigation } from "@react-navigation/native"
 import { Center, Heading, Image, Text, VStack, ScrollView } from '@gluestack-ui/themed'
 
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes"
 
-import { useNavigation } from "@react-navigation/native"
-
-import backgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+import backgroundImg from '@assets/background.png'
+
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
+
+type FormData = {
+  email: string;
+  password: string;
+}
+
 export function SignIn() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
 
   function handNewAccount() {
     navigation.navigate("signUp")
   }
 
+  function handleSignIn({ email, password }: FormData) {
+    console.log(email, password);
+  }
+
   return (
-    <ScrollView 
-    contentContainerStyle={{ flexGrow: 1 }}
-    showsVerticalScrollIndicator={false}
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
     >
-    <VStack flex={1}>
-      <Image
-        w="$full"
-        h={624}
-        source={backgroundImg}
-        defaultSource={backgroundImg}
-        alt="Pessoas treinando"
-        position="absolute"
-      />
-      <VStack flex={1} px="$10" pb="$16">
-        <Center my="$24">
+      <VStack flex={1} px={10} pb={16}>
+        <Image
+          w="$full"
+          h={624}
+          source={backgroundImg}
+          defaultSource={backgroundImg}
+          alt="Pessoas treinando"
+          position="absolute"
+        />
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24">
             <Logo />
             <Text color="$gray100" fontSize="$sm">
-            Treine sua mente e seu corpo
-            </Text>  
-        </Center>
+              Treine sua mente e seu corpo
+            </Text>
+          </Center>
 
-        <Center gap="$2">
-          <Heading color="$gray100">Acesse a conta</Heading>
+          <Center>
+            <Heading color="$gray100" fontSize="$xl" mb={6} fontFamily="$heading">
+              Acesse a conta
+            </Heading>
 
-          <Input 
-           placeholder="E-mail"
-           keyboardType="email-address"
-           autoCapitalize="none"
-           />
+            <Controller
+              control={control}
+              name="email"
+              rules={{ required: 'Informe o e-mail' }}
+              render={({ field: { onChange } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  errorMessage={errors.email?.message}
+                />
+              )}
+            />
 
-          <Input 
-           placeholder="Senha"
-           secureTextEntry
-           />
-          
-          <Button title="Acessar" />
-        </Center>
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: 'Informe a senha' }}
+              render={({ field: { onChange } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
 
-        <Center flex={1} justifyContent="flex-end" mt="$4">
+            <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
+
+          </Center>
+
+          <Center mt={24}>
             <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
               Ainda não tem aceso?
             </Text>
 
             <Button
-             title="Criar Conta"
-             variant="outline"
-             onPress={handNewAccount}/>
+              title="Criar Conta"
+              variant="outline"
+              onPress={handNewAccount} />
           </Center>
 
+        </VStack>
       </VStack>
-
-    </VStack>
     </ScrollView>
   )
 }
