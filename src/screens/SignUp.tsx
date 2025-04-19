b8aca2101df0cf21c45877ac34d68daa2c152076
antonio-cod <antonio_carlostads@hotmail.com>
2025-04-18 20:10:56 -0400

    Creating account and logging in user

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -13,6 +13,8 @@ import { AppError } from '@utils/AppError';
 
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
+import { useState } from 'react';
+import { useAuth } from '@hooks/useAuth';
 
 type FormDataProps = {
   name: string;
@@ -35,8 +37,10 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+const [isLoading, setIsLoading] = useState(false);
 
  const toast = useToast();
+ const { signIn } = useAuth();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema)
@@ -50,9 +54,14 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', {name, email, password});
-      console.log(response.data);
+      setIsLoading(true);
+
+      await api.post('/users', {name, email, password});
+      await signIn(email, password);
+
     } catch (error){
+      setIsLoading(false);
+      
       const isAppError = error instanceof AppError;
       const title = isAppError ? error.message : 'Não foi possivel criar a conta. tente mais tarde.';
 
@@ -167,6 +176,7 @@ export function SignUp() {
           <Button 
             title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isLoading}
           />
 
         </Center>
